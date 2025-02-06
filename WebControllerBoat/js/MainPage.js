ef56906bb6222e4a9439482cdbd2d621439f1157
r4wns <private.nazarmarkov@gmail.com>
2025-02-06 21:38:33 +0100

    update

--- WebControllerBoat/js/MainPage.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("startbutton").addEventListener("click", function () {
-        window.location.href = "controller.html"; // The button redirects you to the controller webpage
-    });
-});
