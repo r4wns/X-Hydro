6c1e602eed75c65a9a4c94a5f9d8edaf79265031
r4wns <private.nazarmarkov@gmail.com>
2025-02-06 22:16:48 +0100

    Updated the code and CSS

--- WebControllerBoat/js/index.js
+++ WebControllerBoat/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("startbutton").addEventListener("click", function () {
-        window.location.href = "controller.html"; // The button redirects you to the controller webpage
+    document.getElementById("startButton").addEventListener("click", function () {
+        window.location.href = "pages/controller.html";
     });
 });
